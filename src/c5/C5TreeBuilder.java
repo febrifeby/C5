c21de74c3f3a8369e905a3585e4807366f761231
dyahayu <dyahayu@nously>
2018-05-27 21:55:39 +0700

    brand new

---
+++ src/c5/C5TreeBuilder.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c5;
+
+/**
+ *
+ * @author dyahayu
+ */
+public class C5TreeBuilder 
+{
+    private Node root;
+    
+    public void initiateBuild()
+    {
+	if ( root == null ) {
+	    build();
+	}
+    }
+    
+    public Node build()
+    {
+	return null;
+    }
+    
+    
+}
